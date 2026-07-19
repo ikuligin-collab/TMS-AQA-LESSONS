92a2ffafd4b03c8f89371f79fa844a3a6efa74f8
ikuligin-collab <i.kuligin@evernode.tech>
2026-07-19 21:57:29 +0200

    Проверки ,что корабль не выходит за границы достки

--- Battleship/Battleship/Board.cs
+++ Battleship/Battleship/Board.cs
@@ -9,9 +9,30 @@ class Board
 
     public Board(int rows, int columns, Ship[] ships)
     {
+        // проверка что ращмеры доски не нулевые
+        if (rows <= 0 || columns <= 0)
+            throw new ArgumentException("Размеры доски должны быть больше нуля.");
         this.Rows = rows;
         this.Columns = columns;
         this.Ships = ships;
+        foreach (var ship in ships)
+        {
+            // Сначала проверяем, что начальная точка вообще на доске
+            if (!IsInside(ship.Position))
+            {
+                throw new ArgumentException("Начало корабля находится вне доски!", nameof(ships));
+            }
+
+            // Проверяем хвост корабля в зависимости от его типа
+            if (ship is HorizontalShip && ship.Position.X + ship.Length > Columns)
+            {
+                throw new ArgumentException("Горизонтальный корабль выходит за границы доски!", nameof(ships));
+            }
+            else if (ship is VerticalShip && ship.Position.Y + ship.Length > Rows)
+            {
+                throw new ArgumentException("Вертикальный корабль выходит за границы доски!", nameof(ships));
+            }
+        }
     }
 
     public bool IsInside(Position position) //  ref if Positions is a class, value if Positions is a struct
